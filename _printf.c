d3eee064c83155678c24c233b3efe2acafe23ace
Anthony Baah <abaah17@alustudent.com>
2020-10-27 12:24:26 +0200

    Implemented the man page and printf

--- _printf.c
+++ _printf.c
@@ -4,11 +4,51 @@
  * _printf - produces output according to a format
  * @format: character string.
  *
- * Return: number of characters printed 
- * 	(excluding the null byte used to end output to strings)
+ * Return: number of characters printed
+ *(excluding the null byte used to end output to strings)
  */
 
 int _printf(const char *format, ...)
 {
+	int counter = -1;
 
+
+	if (format != NULL)
+	{
+		int i;
+		va_list ar_list;
+		int (*o)(va_list);
+
+		va_start(ar_list, format);
+		if (format[0] == '%' && format[1] == '\0')
+		{
+			return (-1);
+		}
+		counter = 0;
+		for (i = 0; format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				if (format[i + 1] == '%')
+				{
+					counter += _putchar(format[i]);
+					i++;
+				}
+				else if (format[i + 1] != '\0')
+				{
+					o = get_func(format[i + 1]);
+					counter += (o ? o(ar_list) :
+						    _putchar(format[i]) +
+						    _putchar(format[i + 1]));
+					i++;
+				}
+			}
+			else
+			{
+				counter += _putchar(format[i]);
+			}
+		}
+		va_end(ar_list);
+	}
+	return (counter);
 }
